f870af2a98938e41ada94b0c854903e6913f847e
unicodepython123 <126125030+unicodepython123@users.noreply.github.com>
2025-04-24 16:29:28 +0700

    second commit

--- imports.py
+++ imports.py
@@ -4,4 +4,4 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 from scipy.stats import f_oneway
-
+from statsmodels.stats.multicomp import pairwise_tukeyhsd
